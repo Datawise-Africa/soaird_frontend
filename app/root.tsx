18d749e430f785b532a412b6e7e4df3022fb7c60
BrianOdhiambo <brianodhiambo530@gmail.com>
2026-08-12 16:12:39 +0300

    updated favicon

--- app/root.tsx
+++ app/root.tsx
@@ -28,6 +28,7 @@ export async function loader({ request }: Route.LoaderArgs) {
 }
 
 export const links: Route.LinksFunction = () => [
+  { rel: 'icon', type: 'image/png', href: '/favicon.png' },
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
     rel: 'preconnect',
